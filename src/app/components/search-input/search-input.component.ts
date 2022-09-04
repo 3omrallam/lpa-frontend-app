75e21aa0c38b8e5a4982a1b85f0f4076c8bddf1a
3omr Allam <76155662+3omrallam@users.noreply.github.com>
2022-09-04 19:53:39 +0200

    update-Form

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SearchOperationService } from 'src/app/services/search-operation.service';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.sass']
 })
 export class SearchInputComponent {
+
   @ViewChild('reset') resetBTN : any
+
+  profileForm = new FormGroup({
+    lang: new FormControl(''),
+  });
   
   constructor(public _SearchOperationService : SearchOperationService){}
 
-  onSubmit(formValue : any){
-    this._SearchOperationService.addTag(formValue)
+  onSubmit(){
+    this._SearchOperationService.addTag(this.profileForm.value)
     this._SearchOperationService.triggerClick(this.resetBTN)
   }
-
-  
 
 }
